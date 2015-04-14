19f61297818d6800b67ad5bfc2873a4d4e471738
Marshall Sorenson <marshall.sorenson@gmail.com>
2015-04-14 14:23:00 -0400

    added custom dashboard template parts INFCB-2

---
+++ src/templates/dashboard/cpanel-start-cbox-theme.php
@@ -0,0 +1,26 @@
+
+<h2>Thank you for using Commons In A Box!</h2>
+
+<p>
+	Welcome to the Theme Dashboard of your CBOX installation.
+	This is where you can find a whole bunch of options to change the front end of your site.
+	Click on the "Options" tab at the top right of this page to view all the customization options available.
+</p>
+
+<?php
+	get_template_part( 'templates/dashboard/cpanel-help', get_template() );
+?>
+
+<h3>
+	Community Support
+</h3>
+
+<p>
+	Please post any questions you have or issues you encounter on our
+	<a href="http://commonsinabox.org/groups/help-support/" target="_blank">Help &amp; Support Forums</a>
+</p>
+
+<p>
+	Please include your <em>installation details</em> when you create a support topic.
+	They are displayed in the side bar of this page <strong>&rarr;</strong>
+</p>
