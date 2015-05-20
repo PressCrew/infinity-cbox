0a3cb46caeb1676b8f843845b80b946871f81594
Marshall Sorenson <marshall.sorenson@gmail.com>
2015-05-20 15:49:27 -0400

    tweaked upgrade logic based on changes to upstream

--- src/engine/custom/upgrade.php
+++ src/engine/custom/upgrade.php
@@ -29,43 +29,9 @@ function cbox_theme_upgrade()
 }
 add_action( 'infinity_dashboard_activated', 'cbox_theme_upgrade' );
 
-
-/**
- * Special actions to perform when ICE is upgraded to v1.2.
- *
- * @param ICE_Upgrade $ice_upgrade
- */
-function cbox_theme_upgrade_ice_1_2( ICE_Upgrade $ice_upgrade )
-{
-	// was ICE upgraded to at least 1.2?
-	if ( true === $ice_upgrade->version_compare( '1.2', '>=' ) ) {
-
-		// yes, rename deprecated options
-		ice_rename_options( array(
-			// buttons
-			'cbox_button_color' => 'buttons.custom-color',
-			// post thumbs
-			'cbox-thumb-height' => 'thumbs.image-height',
-			'cbox-thumb-width' => 'thumbs.image-width',
-			// slider
-			'cbox_flex_slider' => 'slider.mode',
-			'cbox-flex-slider-height' => 'slider.height',
-			'cbox-flex-slider-width' => 'slider.width',
-			'cbox_flex_slider_amount' => 'slider.amount',
-			'cbox_flex_slider_time' => 'slider.time',
-			'cbox_flex_slider_transition' => 'slider.transition',
-			'cbox_flex_slider_category' => 'slider.category'
-		) );
-
-	}
-}
-add_action( 'ice_theme_upgraded',  'cbox_theme_upgrade_ice_1_2' );
-
-
 //
 // Classes
 //
-
 
 /**
  * Upgrades required for version 1.1.
@@ -78,6 +44,23 @@ class CBOX_Upgrade_1_1 extends ICE_Upgrade
 	{
 		// current version less than 1.1?
 		if ( true === $this->version_compare( '1.1', '<' ) ) {
+
+			// rename/import deprecated options
+			ice_rename_options( array(
+				// buttons
+				'cbox_button_color' => 'buttons.custom-color',
+				// post thumbs
+				'cbox-thumb-height' => 'thumbs.image-height',
+				'cbox-thumb-width' => 'thumbs.image-width',
+				// slider
+				'cbox_flex_slider' => 'slider.mode',
+				'cbox-flex-slider-height' => 'slider.height',
+				'cbox-flex-slider-width' => 'slider.width',
+				'cbox_flex_slider_amount' => 'slider.amount',
+				'cbox_flex_slider_time' => 'slider.time',
+				'cbox_flex_slider_transition' => 'slider.transition',
+				'cbox_flex_slider_category' => 'slider.category'
+			) );
 
 			// rename all deprecated slider postmeta keys used in previous versions.
 			ICE_Compat_Posts::rename_postmeta_key( '_cbox_custom_url', 'slider_custom_url' );
